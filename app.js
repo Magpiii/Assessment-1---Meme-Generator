bab3c6698b18fd6be5ae92ab8fbf20205470768a
JB Markowicz <hunterhudson3@gmail.com>
2021-12-13 19:29:01 -0700

    Add form functionality, add functionality to reset user inputs upon form submit, reconfigure meme div so all elements go into one div (bigDiv), style top text, bottom text, and memeDiv of meme for proper display, refactor top and bottom text to be div instead of p elements, import CSS impact font from cited source.

--- app.js
+++ app.js
@@ -12,26 +12,60 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Create elements (including new div). 
-    let div = document.createElement('div');
-    let newTopText = document.createElement('p');
-    let newBottomText = document.createElement('p');
+    let bigDiv = document.createElement('div');
+    let topTextDiv = document.createElement('div');
+    let bottomTextDiv = document.createElement('div');
+    let memeDiv = document.createElement('div');
+    let newTopText = document.createElement('div');
+    let newBottomText = document.createElement('div');
     let img = document.createElement('img');
     let rmBtn = document.createElement('button');
 
     // Assign attributes to dev elements. 
-    newTopText.innerText = topText.value;
-    newBottomText.innerText = bottomText.value;
+    topTextDiv.innerText = topText.value;
+    bottomTextDiv.innerText = bottomText.value;
     img.setAttribute('src', url.value);
     rmBtn.innerText = 'Remove';
 
-    // Append elements to new div. 
-    div.appendChild(newTopText);
-    div.appendChild(newBottomText);
-    div.appendChild(img);
-    div.appendChild(rmBtn);
+    // Append respective text to respective divs. 
+    topTextDiv.append(newTopText);
+    bottomTextDiv.append(newBottomText);
 
-    // Append new div to master div. 
-    masterDiv.appendChild(div);
+    // Set id's. 
+    memeDiv.setAttribute('id', 'meme');
+    topTextDiv.setAttribute('id', 'top-text-div');
+    bottomTextDiv.setAttribute('id', 'bottom-text-div');
+
+    // Append img, topText and bottomText to memeDiv for styling purposes.
+    memeDiv.append(topTextDiv);
+    memeDiv.append(bottomTextDiv);
+    memeDiv.append(img);
+
+    // Style topTextDiv, bottomTextDiv, and memeDiv. 
+    // SOURCE CITED: https://stackoverflow.com/questions/8708945/how-to-position-text-over-an-image-in-css, accessed 13 December 2021. 
+    memeDiv.style.position = 'relative';
+    topTextDiv.style.position = 'absolute';
+    topTextDiv.style.top = '0';
+    topTextDiv.style.left = '160px'
+    topTextDiv.style.textAlign = 'center';
+    topTextDiv.style.fontFamily = '\'Maximum Impact\', sans serif';
+    bottomTextDiv.style.position = 'absolute';
+    bottomTextDiv.style.bottom = '0';
+    bottomTextDiv.style.left = '160px';
+    bottomTextDiv.style.textAlign = 'center';
+    bottomTextDiv.style.fontFamily = '\'Maximum Impact\', sans serif';
+
+    // Append memeDiv and rmBtn to bigDiv. 
+    bigDiv.append(memeDiv);
+    bigDiv.append(rmBtn);
+
+    // Append new bigDiv to masterDiv. 
+    masterDiv.appendChild(bigDiv);
+
+    // Clear inputs on submit. 
+    topText.value = '';
+    bottomText.value= '';
+    url.value = '';
 });
 
 // Delegate masterDiv. 
